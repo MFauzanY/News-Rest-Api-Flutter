4d6fe19b89d9b186650e94feae253716b04ddea8
MFauzanY <56360545+Millshake@users.noreply.github.com>
2020-12-13 20:32:31 +0700

    Update strings.dart

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,4 +1,4 @@
 class Strings {
   static String news_url =
-      'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=9dd907701c9146469b2316acb8eb3c53';
+      'http://newsapi.org/v2/everything?domains=wsj.com&apiKey= ISI PAKE API KEY SENDIRI YA';
 }
